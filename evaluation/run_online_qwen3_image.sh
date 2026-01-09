6043bd45c0104704f9732702f08a876489c807c2
sabreshao <sabre.shao@amd.com>
2026-01-09 18:24:42 +0800

    [script] Update to lora enabled 8steps and 1GPU. (#145)

--- evaluation/run_online_qwen3_image.sh
+++ evaluation/run_online_qwen3_image.sh
@@ -1,1 +1,1 @@
-sglang serve --model-path Qwen/Qwen-Image-Edit-2511 --num-gpus 4 --ulysses-degree=4 --image-encoder-precision bf16 --vae-precision bf16  --host 0.0.0.0 --port 30000
+sglang serve --model-path Qwen/Qwen-Image-Edit-2511 --lora_path lightx2v/Qwen-Image-Edit-2511-Lightning  --num-gpus 1 --ulysses-degree=1 --image-encoder-precision bf16 --vae-precision bf16  --host 0.0.0.0 --port 30000
